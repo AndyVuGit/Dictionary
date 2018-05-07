29c3133baa6ef2f5ac44064296c075a09db74baa
Andy Vu <phucvu95@gmail.com>
2018-05-07 13:02:37 -0700

    Complete Dictionary
    This version uses hashmapping to store data inside the program.

--- dictionary.java
+++ dictionary.java
@@ -1,35 +1,55 @@
-import java.sql.*;
-import java.util.*;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-import com.mysql.jdbc.MysqlDataTruncation;
-import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
-
-public class Driver {
-
-    public static void main(String[] arg) throws Exception
-    {
-      Class.forNAme("com.mysql.jdbc.Driver").newInstance();
-      Connection connection;
-      Statement select;
-
-      try{
-        connection = DriverManager.getConnection("jdbcmysql:///words", "root", "password");
-        select = connection.createStatement();
-
-        String userInput;
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Enter your word: ");
-        userInput = reader.nextLine();
-
-        Statement sqlCommand = connection.createStatement();
-        sqlCommand.executeQuery("SELECT japanese FROM words WHERE english = \"" + userInput + "\"");
-
-        reader.close();
-      }
-      catch (SQLException e)
-      {
-        System.out.println("Error occured");
-      }
-    }
+public class Dictionary {
+	public static void main(String[] args) throws Exception
+	{
+		//Rundown of what will entail
+		/*
+		 * Get data.
+		 * use hashing to store words with their japanese version. Problem is how to store the japanese version. Solution: store in pairs.
+		 * Take in a word and search for it in our own data. 
+		 * return the japanese version of the word.
+		*/
+		
+		//Openning the file
+		File file = new File("data.txt");
+		Scanner scanner = new Scanner(file);
+		
+		Map<String, String> dictMap = new HashMap<String, String>();
+		while (scanner.hasNextLine())
+		{
+			//------------Import Data-------------
+			String engWord = scanner.next();
+			String japWord = scanner.next();
+			
+			//------------Hashing Data------------
+			dictMap.put(engWord, japWord);
+			scanner.nextLine();
+		}
+		
+		scanner.close();
+		
+		
+		//--------Get user input on what word to search up--------
+		Scanner input = new Scanner(System.in);
+		String key = "";
+		while(!key.equals("quit")){
+			key = input.next();
+			if(key.equals("quit")){
+				break;
+			}
+			
+			if(dictMap.get(key) == null){
+				System.out.println("The word is not in our database");
+			}
+			else{
+				System.out.println(dictMap.get(key));
+			}
+		}
+		
+		input.close();
+	}
 }
